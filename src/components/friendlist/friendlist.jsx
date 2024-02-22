c2959c00052773c6583d5a6ef1d738c8cfc5e3d3
DurkaMateusz <mosca888@gmail.com>
2024-02-22 01:05:16 +0100

    fixed friend list visibility status
    fixed visibility of status green/red dot

--- src/components/friendlist/friendlist.jsx
+++ src/components/friendlist/friendlist.jsx
@@ -4,7 +4,7 @@ import styles from './friendlist.module.css';
 
 const FriendListItem = ({avatar, name, isOnline}) => (
 <li className={styles.item}>
-  <span className={styles.status+ '' +isOnline ? styles.online : styles.offline}></span>
+  <span className={isOnline ? styles.online : styles.offline}></span>
   <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
   <p className={styles.name}>{name}</p>
 </li>
